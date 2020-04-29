fcada77b21f8af719da6cc59e1bbbdb0bff005b9
Ali Dusunen <dusunenali@yahoo.com>
2020-04-29 15:42:42 +0300

    django_heroku downloaded and setup

--- sim/settings.py
+++ sim/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +133,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+django_heroku.settings(locals())
